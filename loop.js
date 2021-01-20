adb68b8d81abb4bdf923e8148af0cbc9a2d38f4b
benzkittisak <6206021621082@fitm.kmutnb.ac.th>
2021-01-20 18:15:42 +0700

    update

--- loop.js
+++ loop.js
@@ -23,3 +23,10 @@ for(let name of names){
 }
 
 // do while loop **sniplet
+console.log('-- do while --')
+index = 0;
+do{
+    const name = names[index];
+    console.log(name);
+    index++;
+}while(index<names.length)
